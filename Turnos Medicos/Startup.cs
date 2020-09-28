de844d5264cbcf67dbbca171da835ead61198c65
Hernán <Hernn@MYPCPERSONAL>
2020-09-28 10:26:04 -0300

    Proyecto Inicial

---
+++ Turnos Medicos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Turnos_Medicos.Startup))]
+namespace Turnos_Medicos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

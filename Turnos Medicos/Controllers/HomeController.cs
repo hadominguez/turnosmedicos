dd1a4b893d271be0468b5793b5682b9fa551a9dc
julian9510 <juliange95@gmail.com>
2020-10-22 16:17:39 -0300

    Nuevas Pantallas corregidas
    Se corrigieron varios bugs de front end

--- Turnos Medicos/Controllers/HomeController.cs
+++ Turnos Medicos/Controllers/HomeController.cs
@@ -29,9 +29,23 @@ namespace Turnos_Medicos.Controllers
 
         public ActionResult Contact()
         {
-                ViewBag.Message = "Ponete en contacto con nosostros.";
+                ViewBag.Message = "Ponete en contacto con nosotros.";
 
                 return View();
+        }
+
+        public ActionResult EspecialidadesDisponibles()
+        {
+            ViewBag.Message = "Este es nuestro conjunto de especialidades que tenemos para ofrecerte.";
+
+            return View();
+        }
+
+        public ActionResult ProfesionalesDisponibles()
+        {
+            ViewBag.Message = "Este es nuestro grupo de Profesionales.";
+
+            return View();
         }
     }
 }
